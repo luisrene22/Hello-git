ebdc06b96b177487a2181f7379eede86ff335ae3
ReneOrozco <reneg@ciencias.unam.mx>
2023-07-18 20:43:11 -0600

    ejercicios

---
+++ Python/08_ejercicio11.py
@@ -0,0 +1,10 @@
+'''
+Ejercicio 11
+Haz un proograma que pida el nombre de una persona y lo muestre
+1000 veces, pero dejando un espacio de separación entre cada copia del nombre.
+'''
+nombre = input("Ingresa tu nombre: ")
+espacio = " "
+cadena_formateada = espacio + nombre
+print(cadena_formateada*1000)
+
